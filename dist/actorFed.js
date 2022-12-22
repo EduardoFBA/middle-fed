7c225da32342adc6e5acc635978e22960df2589f
EduardoFBA <fba.eduardo@protonmail.com>
2022-12-22 15:49:11 -0300

    Testing follow request

--- dist/actorFed.js
+++ dist/actorFed.js
@@ -8,16 +8,46 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.actorFedRouter = void 0;
 const express_1 = require("express");
 const utils_1 = require("./utils");
 const utils_json_1 = require("./utils-json");
 exports.actorFedRouter = (0, express_1.Router)();
-exports.actorFedRouter.get("/authorize_interaction", (req, res) => {
-    console.log("authorize interaction", req.body);
-    res.send((0, utils_json_1.createAcceptActivity)(`${req.params.username}@${req.app.get("localDomain")}`, req.body.target, "Follow"));
-});
+exports.actorFedRouter.get("/authorize_interaction", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const buf = yield buffer(req);
+    const rawBody = buf.toString("utf8");
+    console.log("authorize interaction", rawBody);
+    res.send((0, utils_json_1.createAcceptActivity)(`${req.params.uri}@${req.app.get("localDomain")}`, req.body.target, "Follow"));
+}));
+function buffer(readable) {
+    var readable_1, readable_1_1;
+    var e_1, _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        const chunks = [];
+        try {
+            for (readable_1 = __asyncValues(readable); readable_1_1 = yield readable_1.next(), !readable_1_1.done;) {
+                const chunk = readable_1_1.value;
+                chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (readable_1_1 && !readable_1_1.done && (_a = readable_1.return)) yield _a.call(readable_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return Buffer.concat(chunks);
+    });
+}
 exports.actorFedRouter.get("/u/:username", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const domain = req.app.get("localDomain");
     const result = yield (0, utils_1.search)("actor", "id", `https://${domain}/u/${req.params.username}`);
--- src/actorFed.ts
+++ src/actorFed.ts
@@ -1,19 +1,33 @@
 import { Request, Response, Router } from "express";
+import { Readable } from "stream";
 import { search, getWebfinger, save, list } from "./utils";
 import { createAcceptActivity } from "./utils-json";
 
 export const actorFedRouter = Router();
 
-actorFedRouter.get("/authorize_interaction", (req: Request, res: Response) => {
-  console.log("authorize interaction", req.body);
-  res.send(
-    createAcceptActivity(
-      `${req.params.username}@${req.app.get("localDomain")}`,
-      req.body.target,
-      "Follow"
-    )
-  );
-});
+actorFedRouter.get(
+  "/authorize_interaction",
+  async (req: Request, res: Response) => {
+    const buf = await buffer(req);
+    const rawBody = buf.toString("utf8");
+    console.log("authorize interaction", rawBody);
+    res.send(
+      createAcceptActivity(
+        `${req.params.uri}@${req.app.get("localDomain")}`,
+        req.body.target,
+        "Follow"
+      )
+    );
+  }
+);
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
 
 actorFedRouter.get("/u/:username", async (req: Request, res: Response) => {
   const domain = req.app.get("localDomain");
